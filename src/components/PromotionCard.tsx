faa3ca73663f1470711d24d55abc2d5abd7b5324
inhye94 <inhye.park.dev@gmail.com>
2025-03-22 17:27:50 +0900

    style(feature): PromotionCard 반응형 퍼블리싱

--- src/components/PromotionCard.tsx
+++ src/components/PromotionCard.tsx
@@ -1,23 +1,25 @@
 import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { breakpoints } from "../constants/breakpoints";
 import { colors } from "../constants/colors";
+import type { IPromotionType } from "../types/promotion";
+import Badge from "./Badge";
 
-export default function PromotionCard() {
+export default function PromotionCard({
+  promotion,
+}: {
+  promotion: IPromotionType;
+}) {
   return (
-    <article
-      css={css`
-        display: flex;
-        flex-direction: column-reverse;
-        border-radius: 8px;
-        background-color: white;
-        box-shadow: 0 1px 5px 0 #ddd;
-        overflow: hidden;
-      `}
-    >
-      <header
-        css={css`
-          padding: 8px;
-        `}
-      >
+    <Card>
+      {promotion.linkUrl && (
+        <PromotionLink
+          href={promotion.linkUrl}
+          target="_blank"
+          aria-label={`${promotion.description} 페이지로 이동`}
+        />
+      )}
+      <TextBox>
         <div
           css={css`
             display: flex;
@@ -26,79 +28,104 @@ export default function PromotionCard() {
             margin-bottom: 4px;
           `}
         >
-          <h3
-            css={css`
-              font-size: 12px;
-              font-weight: 700;
-              max-width: 65%;
-              text-overflow: ellipsis;
-              overflow: hidden;
-              white-space: nowrap;
-            `}
-          >
-            [M COUNTDOWN] 10월 2주차 엠카 사전투표 결과보기
-          </h3>
-          <strong
-            css={css`
-              padding: 0 8px;
-              font-size: 10px;
-              font-weight: 900;
-              border: 1px solid ${colors.pink50};
-              border-radius: 100px;
-              color: ${colors.pink50};
-            `}
-          >
-            투표하기
-          </strong>
+          <PromotionTitle>{promotion.title}</PromotionTitle>
+          {promotion.action && (
+            <Badge variant="outlined" shape="pill" size={10}>
+              {promotion.action}
+            </Badge>
+          )}
         </div>
-        <p
-          css={css`
-            font-size: 10px;
-            text-align: right;
-          `}
-        >
-          2020.02.09 10:00 ~ 2020.04.08 17:00 (KST)
-        </p>
-      </header>
-      <div
-        css={css`
-          position: relative;
-          width: 100%;
-          aspect-ratio: 21/9;
-        `}
-      >
-        <div
-          css={css`
-            position: absolute;
-            left: 8px;
-            top: 8px;
-          `}
-        >
-          <strong
-            css={css`
-              padding: 1px 2px;
-              font-size: 12px;
-              font-weight: 900;
-              border: 1px solid ${colors.pink50};
-              border-radius: 4px;
-              color: ${colors.common100};
-              background-color: ${colors.pink50};
-            `}
-          >
-            진행 중
-          </strong>
-        </div>
-        <img
-          css={css`
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            background-color: ${colors.neutral95};
-          `}
-          src=""
-          alt="엠카운트다운 글로벌 사전 투표"
-        />
-      </div>
-    </article>
+        <PromotionDate>{promotion.period}</PromotionDate>
+      </TextBox>
+      <ImageBox>
+        <BadgeWrapper>
+          {promotion.state && <Badge>{promotion.state}</Badge>}
+        </BadgeWrapper>
+        <PromotionImage src={promotion.imageSrc} alt={promotion.description} />
+      </ImageBox>
+    </Card>
   );
 }
+
+const Card = styled.article`
+  position: relative;
+  display: flex;
+  flex-direction: column-reverse;
+  border-radius: 8px;
+  background-color: white;
+  box-shadow: 0 1px 5px 0 #ddd;
+  overflow: hidden;
+`;
+
+// link
+const PromotionLink = styled.a`
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
+
+// Text Box
+const TextBox = styled.header`
+  padding: 8px;
+
+  @media screen and (min-width: ${breakpoints.lg}px) {
+    padding: 16px;
+  }
+`;
+
+const PromotionTitle = styled.h3`
+  font-size: 12px;
+  font-weight: 700;
+  max-width: 65%;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+
+  @media screen and (min-width: ${breakpoints.lg}px) {
+    font-size: 16px;
+  }
+`;
+
+const PromotionDate = styled.p`
+  font-size: 10px;
+  text-align: right;
+
+  @media screen and (min-width: ${breakpoints.lg}px) {
+    font-size: 12px;
+  }
+`;
+
+// ImageBox
+const ImageBox = styled.div`
+  position: relative;
+  width: 100%;
+  aspect-ratio: 21/9;
+
+  @media screen and (min-width: ${breakpoints.lg}px) {
+    width: 100%;
+    height: 300px;
+    aspect-ratio: unset;
+  }
+`;
+
+const BadgeWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  padding: 8px;
+
+  @media screen and (min-width: ${breakpoints.lg}px) {
+    padding: 16px;
+  }
+`;
+
+const PromotionImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  background-color: ${colors.neutral95};
+`;
