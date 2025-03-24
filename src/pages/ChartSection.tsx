9bbf00ac1d4ff97bfe52821c3f1b972b6610ec84
inhye94 <inhye.park.dev@gmail.com>
2025-03-24 10:59:17 +0900

    refactor(feature): useInfiniteScroll 적용

--- src/pages/ChartSection.tsx
+++ src/pages/ChartSection.tsx
@@ -1,77 +1,34 @@
-import { useCallback, useEffect, useRef, useState } from "react";
 import { getMockData } from "../apis/product";
 import Article from "../components/Article";
 import ChartItem from "../components/ChartItem";
 import ChartList from "../components/ChartList";
+import ChartSkeleton from "../components/ChartSkeleton";
+import Empty from "../components/Empty";
 import Loading from "../components/Loading";
-import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
-import type { IMockData } from "../types/mockData";
+import useInfiniteScroll from "../hooks/useInfiniteScroll";
 
 export default function ChartSection() {
-  const [list, setList] = useState<IMockData[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [pageNum, setPageNum] = useState<number>(0);
-  const [isDataEnd, setIsDataEnd] = useState<boolean>(false);
-  const targetRef = useRef<HTMLDivElement>(null);
-
-  const handleObserver = useCallback(() => {
-    if (!isDataEnd && !isLoading) {
-      setPageNum((prev) => prev + 1);
-    }
-  }, [isDataEnd, isLoading]);
-
-  const { observe, unobserve } = useIntersectionObserver(handleObserver);
-
-  const loadData = useCallback(
-    async (pageNumber: number) => {
-      if (isDataEnd) return;
-
-      // 로딩 실행
-      setIsLoading(true);
-
-      // 데이터 업데이트
-      const { datas, isEnd } = await getMockData(pageNumber);
-      setList((prev) => prev.concat(...datas));
-      setIsDataEnd(isEnd);
-
-      // 로딩 멈춤
-      setIsLoading(false);
-    },
-    [isDataEnd]
-  );
-
-  // 옵저버 설정
-  useEffect(() => {
-    if (!targetRef.current) return;
-
-    observe(targetRef.current);
-
-    return () => {
-      if (targetRef.current) {
-        unobserve(targetRef.current);
-      }
-    };
-  }, [isLoading, isDataEnd]);
-
-  // 데이터 로드
-  useEffect(() => {
-    loadData(pageNum);
-  }, [pageNum]);
+  const { isLoading, list, targetRef } = useInfiniteScroll({
+    fetchData: getMockData,
+  });
 
   return (
     <Article title="실시간 차트">
-      <ChartList>
-        {list &&
-          list?.map((item, i) => (
+      {list && list.length === 0 && <Empty />}
+      {list && list.length > 0 && (
+        <ChartList>
+          {list?.map((item, i) => (
             <ChartItem
-              index={i}
-              key={i}
+              key={item.productId}
               product={item}
+              index={i}
               onClick={() => alert(123)}
             />
           ))}
-      </ChartList>
+        </ChartList>
+      )}
       <div ref={targetRef}></div>
+      {!list && <ChartSkeleton length={3} />}
       {isLoading && <Loading />}
     </Article>
   );
