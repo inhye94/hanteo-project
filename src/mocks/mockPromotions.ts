f6879621880d879d349bba9a3b1ceab0e52d27d5
inhye94 <inhye.park.dev@gmail.com>
2025-03-24 11:58:54 +0900

    fix(main): 프로모션 이미지 주소 변경

--- src/mocks/mockPromotions.ts
+++ src/mocks/mockPromotions.ts
@@ -6,8 +6,7 @@ export const mockPromotions: IPromotionType[] = [
     title: "[M COUNTDOWN] 10월 2주차 엠카 사전투표 결과보기",
     period: "2020.02.09 10:00 ~ 2020.04.08 17:00 (KST)",
     action: "투표하기",
-    imageSrc:
-      "https://private-user-images.githubusercontent.com/69448900/418059076-27c892a2-e11c-4a36-b3b7-bd83b0e6d63b.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDI2MzM4MzYsIm5iZiI6MTc0MjYzMzUzNiwicGF0aCI6Ii82OTQ0ODkwMC80MTgwNTkwNzYtMjdjODkyYTItZTExYy00YTM2LWIzYjctYmQ4M2IwZTZkNjNiLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAzMjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMzIyVDA4NTIxNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWEyMzQ2YTJhY2MyYTQ4MmZjOTIzNmRhNjUxYzgzZmVkZWNhZDkwMmIzMDhhODhhNTM4ZjcxMjUzYzU5ZWQ0YzImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.D_PL-C9Pju69eJMG-PjWBFoNUl8iaFZoooRnefmFYZw",
+    imageSrc: "/thumb_portfolio.jpg",
     description: "엠카운트다운 글로벌 사전 투표",
     state: "진행 중",
     linkUrl: "https://inhye-portfolio.vercel.app/",
@@ -16,8 +15,7 @@ export const mockPromotions: IPromotionType[] = [
     id: "promotion-2",
     title: "[M COUNTDOWN] 10월 2주차 엠카 사전투표 결과보기",
     period: "2020.02.09 10:00 ~ 2020.04.08 17:00 (KST)",
-    imageSrc:
-      "https://private-user-images.githubusercontent.com/69448900/403272271-1bc245a9-ec5b-4db9-8739-5efc976f1b21.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDI2MzM4NjIsIm5iZiI6MTc0MjYzMzU2MiwicGF0aCI6Ii82OTQ0ODkwMC80MDMyNzIyNzEtMWJjMjQ1YTktZWM1Yi00ZGI5LTg3MzktNWVmYzk3NmYxYjIxLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAzMjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMzIyVDA4NTI0MlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZkMjU1ODI4YzE4YmY2ZTBlNGE4ZmYyOTg5NjNmNGY5MDE2ZjQzMDllNzFmMWJmNTc0NDQ0MTdjYWE5YWE3MTUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.21cCB-KNLc45icAOZzuhlrZVfER_F7lmYb0A67_oFgA",
+    imageSrc: "/thumb_designsystem.jpg",
     description: "엠카운트다운 글로벌 사전 투표",
     state: "진행 중",
     linkUrl: "https://inhye-portfolio.vercel.app/",
@@ -26,8 +24,7 @@ export const mockPromotions: IPromotionType[] = [
     id: "promotion-3",
     title: "[M COUNTDOWN] 10월 2주차 엠카 사전투표 결과보기",
     period: "2020.02.09 10:00 ~ 2020.04.08 17:00 (KST)",
-    imageSrc:
-      "https://private-user-images.githubusercontent.com/69448900/401784817-099ac2ab-9542-4715-ad1e-64e62fba26a3.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDI2MzM5MTMsIm5iZiI6MTc0MjYzMzYxMywicGF0aCI6Ii82OTQ0ODkwMC80MDE3ODQ4MTctMDk5YWMyYWItOTU0Mi00NzE1LWFkMWUtNjRlNjJmYmEyNmEzLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAzMjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMzIyVDA4NTMzM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTNkNTQ3NzZmOWIwNjYzZmJiNDU4Mjc2MDhkMjFhOWY0YmE0NTY2NmQ2MDY5MTQ3NGU5ZGViMzhjOGYzZGE3NmYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.eMQBBsor_9tlAmRpxjwe-Vdaq7IdbNBpD7E4f5BghgQ",
+    imageSrc: "/thumb_ott-search.jpg",
     description: "엠카운트다운 글로벌 사전 투표",
     state: "D-1",
     linkUrl: "https://inhye-portfolio.vercel.app/",
